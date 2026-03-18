0552295bf026226db2d9eb4844f463c7928e48b3
Apoorav Rathore <apoorav.rathore@icloud.com>
2026-03-18 11:38:10 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+
+    const handleSubmit = async (e) => {
+	e.preventDefault();
+    }
+
+  return (
+<div className="main">
+  <h1><i>scheduler</i></h1>
+
+  <form onSubmit="handleSubmit">
+    
+  </form>
+</div>
+  );
+}
+
+export default App;
